64467e46f84bf01651a4d83f276c4b19bbf2b066
dmitry1721 <gasanyandmitry@yandex.ru>
2023-04-21 10:41:00 +0300

    Public commit

---
+++ routes/api.php
@@ -0,0 +1,83 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+
+});
+
+Route::group(['namespace' => '\App\Http\Controllers\Api\User', 'prefix' => 'users'], function (){
+    Route::post('/phone_auth', 'UserController@phone_auth')->name('api.users.phone_auth');
+    Route::post('/register', 'UserController@register')->name('api.users.register');
+    Route::post('/login/{user:phone}', 'UserController@login')->name('api.users.login');
+    Route::get('/userIpInfo', 'UserController@getIpInfo')->name('api.users.getIpInfo');
+
+});
+Route::group(['middleware'=>['auth:sanctum', 'json',]], function (){
+
+    Route::group(['namespace' => '\App\Http\Controllers\Api\User', 'prefix' => 'users'], function (){
+        Route::get('/user/{user}', 'UserController@index')->name('api.users.index');
+        Route::post('/{user}', 'UserController@update')->name('api.users.update');
+        Route::get('/logout/{user}', 'UserController@logout')->name('api.users.logout');
+    });
+    Route::group(['namespace' => '\App\Http\Controllers\Api\Message', 'prefix' => 'messages'], function (){
+        Route::post('/{user}/create', 'MessageController@create')->name('api.messages.create');
+    });
+    Route::group(['namespace' => '\App\Http\Controllers\Api\Basket', 'prefix' => 'baskets'], function (){
+        Route::post('/{user}', 'BasketController@update')->name('api.baskets.update');
+        Route::get('/{user}', 'BasketController@index')->name('api.baskets.index');
+//        Route::get('/count/{user}', 'BasketController@count')->name('api.baskets.count');
+        Route::delete('/{user}', 'BasketController@destroy')->name('api.baskets.destroy');
+    });
+    Route::group(['namespace' => '\App\Http\Controllers\Api\Favorite', 'prefix' => 'favorites'], function (){
+        Route::post('/{user}', 'FavoriteController@update')->name('api.favorites.update');
+        Route::get('/{user}', 'FavoriteController@index')->name('api.favorites.index');
+        Route::delete('/{user}', 'FavoriteController@destroy')->name('api.favorites.destroy');
+    });
+    Route::group(['namespace' => '\App\Http\Controllers\Api\Compare', 'prefix' => 'compares'], function (){
+        Route::post('/{user}', 'CompareController@update')->name('api.compares.update');
+        Route::get('/{user}', 'CompareController@index')->name('api.compares.index');
+        Route::get('/category/{user}', 'CompareController@category')->name('api.compares.category');
+        Route::delete('/{user}', 'CompareController@destroy')->name('api.compares.destroy');
+    });
+});
+
+Route::group(['namespace' => '\App\Http\Controllers\Api\Product', 'prefix' => 'products'], function (){
+    Route::get('/', 'ProductController@index')->name('api.products.index');
+    Route::get('/{slug}', 'ProductController@show')->name('api.products.show');
+});
+
+Route::group(['namespace' => '\App\Http\Controllers\Api\Tag', 'prefix' => 'tags'], function (){
+    Route::get('/{tag:slug}', 'TagController@index')->name('api.tags.index');
+});
+
+Route::group(['namespace' => '\App\Http\Controllers\Api\Brand', 'prefix' => 'brands'], function (){
+    Route::get('/', 'BrandController@index')->name('api.brands.index');
+});
+
+Route::group(['namespace' => '\App\Http\Controllers\Api\Category', 'prefix' => 'categories'], function (){
+    Route::get('/', 'CategoryController@index')->name('api.categories.index');
+    Route::get('/{category:slug}', 'CategoryController@show')->name('api.categories.show');
+    Route::get('/price/{category:name}', 'CategoryController@prices')->name('api.categories.prices');
+    Route::get('/category/{id}', 'CategoryController@subCategories')->name('api.categories.subCategories');
+});
+
+//Route::group(['namespace' => '\App\Http\Controllers\Api\User', 'prefix' => 'users'], function (){
+////    Route::get('/', 'CategoryController@index')->name('api.categories.index');
+//    Route::post('/registration', 'UserController@store')->name('api.users.store');
+//    Route::post('/', 'UserController@index')->name('api.users.index');
+//});
+
---
+++ routes/web.php
@@ -0,0 +1,135 @@
+<?php
+
+use App\Notifications\Telegram;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Notification;
+
+
+//Route::get('/admin/login/Erw12sd', function () {
+//    return view('auth.login');
+//})->name('login');
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
+Auth::routes();
+
+//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+//
+Route::group(['namespace' => '\App\Http\Controllers\Admin', 'prefix' => 'admin', 'middleware' => 'admin'], function (){
+
+    Route::get('/', IndexController::class)->name('admin.index');
+
+    Route::group(['namespace' => '\App\Http\Controllers\Admin\Profile','prefix' => 'profiles'], function (){
+
+        Route::get('/{profile:slug}', 'ProfileController@index')->name('admin.profiles.index');
+        Route::patch('/{profile:slug}/{user}', 'ProfileController@update')->name('admin.profiles.update');
+    });
+
+    Route::group(['namespace' => '\App\Http\Controllers\Admin\User', 'prefix' => 'users'], function (){
+
+        Route::get('/', 'UserController@index')->name('admin.users.index');
+        Route::get('/create', 'UserController@create')->name('admin.users.create');
+        Route::post('/', 'UserController@store')->name('admin.users.store');
+    });
+
+
+    Route::group(['namespace' => '\App\Http\Controllers\Admin\Category', 'prefix' => 'categories'], function (){
+
+        Route::get('/', 'CategoryController@index')->name('admin.categories.index');
+        Route::get('/create', 'CategoryController@create')->name('admin.categories.create');
+        Route::post('/', 'CategoryController@store')->name('admin.categories.store');
+        Route::get('/{category:slug}', 'CategoryController@show')->name('admin.categories.show');
+        Route::get('/{category:slug}/edit', 'CategoryController@edit')->name('admin.categories.edit');
+        Route::patch('/{category:slug}', 'CategoryController@update')->name('admin.categories.update');
+        Route::delete('/{category:slug}', 'CategoryController@destroy')->name('admin.categories.destroy');
+    });
+
+    Route::group(['namespace' => '\App\Http\Controllers\Admin\Brand', 'prefix' => 'brands'], function (){
+
+        Route::get('/', 'BrandController@index')->name('admin.brands.index');
+        Route::get('/create', 'BrandController@create')->name('admin.brands.create');
+        Route::post('/', 'BrandController@store')->name('admin.brands.store');
+        Route::get('/{brand:slug}', 'BrandController@show')->name('admin.brands.show');
+        Route::get('/{brand:slug}/edit', 'BrandController@edit')->name('admin.brands.edit');
+        Route::patch('/{brand:slug}', 'BrandController@update')->name('admin.brands.update');
+        Route::delete('/{brand:slug}', 'BrandController@destroy')->name('admin.brands.destroy');
+
+    });
+
+    Route::group(['namespace' => '\App\Http\Controllers\Admin\Tag', 'prefix' => 'tags'], function (){
+
+        Route::get('/', 'TagController@index')->name('admin.tags.index');
+        Route::get('/create', 'TagController@create')->name('admin.tags.create');
+        Route::post('/', 'TagController@store')->name('admin.tags.store');
+        Route::get('/{tag:slug}', 'TagController@show')->name('admin.tags.show');
+        Route::get('/{tag:slug}/edit', 'TagController@edit')->name('admin.tags.edit');
+        Route::patch('/{tag:slug}', 'TagController@update')->name('admin.tags.update');
+        Route::delete('/{tag:slug}', 'TagController@destroy')->name('admin.tags.destroy');
+
+    });
+
+    Route::group(['namespace' => '\App\Http\Controllers\Admin\Product', 'prefix' => 'products'], function (){
+
+        Route::get('/', 'ProductController@index')->name('admin.products.index');
+        Route::get('/create', 'ProductController@create')->name('admin.products.create');
+        Route::post('/', 'ProductController@store')->name('admin.products.store');
+        Route::get('/{product:slug}/edit', 'ProductController@edit')->name('admin.products.edit');
+        Route::patch('/{product:slug}', 'ProductController@update')->name('admin.products.update');
+        Route::get('/{product:slug}', 'ProductController@show')->name('admin.products.show');
+
+        Route::get('/{product:slug}/{variant_slug}', 'ProductController@variant_show')->name('admin.products.variant_show');
+        Route::get('/{product:slug}/{variant_slug}/edit', 'ProductController@variant_edit')->name('admin.products.variant_edit');
+        Route::patch('/{product:slug}/{variant_slug}', 'ProductController@variant_update')->name('admin.products.variant_update');
+        Route::delete('/{product:slug}', 'ProductController@destroy')->name('admin.products.destroy');
+        Route::delete('/{product:slug}/{variant_slug}', 'ProductController@variant_destroy')->name('admin.products.variant_destroy');
+
+        Route::group(['namespace' => '\App\Http\Controllers\Admin\Product\Property', 'prefix' => 'properties'], function (){
+            Route::get('/', 'PropertyController@index')->name('admin.properties.index');
+            Route::get('/create', 'PropertyController@create')->name('admin.properties.create');
+            Route::post('/', 'PropertyController@store')->name('admin.properties.store');
+            Route::get('/{property:slug}', 'PropertyController@show')->name('admin.properties.show');
+            Route::get('/{property:slug}/edit', 'PropertyController@edit')->name('admin.properties.edit');
+            Route::patch('/{property:slug}', 'PropertyController@update')->name('admin.properties.update');
+            Route::delete('/{property:slug}', 'PropertyController@destroy')->name('admin.properties.destroy');
+        });
+    });
+
+    Route::group(['namespace' => '\App\Http\Controllers\Admin\MainImage', 'prefix' => 'images/main'], function (){
+        Route::get('/', 'MainImageController@index')->name('admin.main_images.index');
+        Route::get('/create', 'MainImageController@create')->name('admin.main_images.create');
+        Route::post('/', 'MainImageController@store')->name('admin.main_images.store');
+        Route::get('/edit', 'MainImageController@edit')->name('admin.main_images.edit');
+        Route::patch('/update', 'MainImageController@update')->name('admin.main_images.update');
+    });
+    Route::group(['namespace' => '\App\Http\Controllers\Admin\BannerImage', 'prefix' => 'images/banner'], function (){
+        Route::get('/', 'BannerImageController@index')->name('admin.banner_images.index');
+        Route::get('/create', 'BannerImageController@create')->name('admin.banner_images.create');
+        Route::post('/', 'BannerImageController@store')->name('admin.banner_images.store');
+        Route::get('/edit', 'BannerImageController@edit')->name('admin.banner_images.edit');
+        Route::patch('/update', 'BannerImageController@update')->name('admin.banner_images.update');
+    });
+    Route::group(['namespace' => '\App\Http\Controllers\Admin\PromotionImage', 'prefix' => 'images/promotion'], function (){
+        Route::get('/', 'PromotionImageController@index')->name('admin.promotion_images.index');
+        Route::get('/create', 'PromotionImageController@create')->name('admin.promotion_images.create');
+        Route::post('/', 'PromotionImageController@store')->name('admin.promotion_images.store');
+        Route::get('/edit', 'PromotionImageController@edit')->name('admin.promotion_images.edit');
+        Route::patch('/update', 'PromotionImageController@update')->name('admin.promotion_images.update');
+    });
+    Route::group(['namespace' => '\App\Http\Controllers\Admin\Logo', 'prefix' => 'logo'], function (){
+        Route::get('/edit', 'LogoController@edit')->name('admin.logo.edit');
+        Route::patch('/{logo}', 'LogoController@update')->name('admin.logo.update');
+    });
+    Route::group(['namespace' => '\App\Http\Controllers\Admin\MyWarehouse', 'prefix' => 'my-warehouse'], function (){
+        Route::get('/', 'MyWarehouseController@index')->name('admin.my-warehouse.index');
+        Route::get('/main', 'MyWarehouseController@main')->name('admin.my-warehouse.main');
+        Route::get('/edit', 'MyWarehouseController@edit')->name('admin.my-warehouse.edit');
+        Route::post('/connect', 'MyWarehouseController@connect')->name('admin.my-warehouse.connect');
+        Route::post('/export', 'MyWarehouseController@export')->name('admin.my-warehouse.export');
+        Route::post('/webhook', 'MyWarehouseController@webhook')->name('admin.my-warehouse.webhook');
+        Route::patch('/{myWarehouse}', 'MyWarehouseController@update')->name('admin.my-warehouse.update');
+    });
+});
+
+//Notification::route('telegram', env('TELEGRAM_CHAT_ID'))
+//    ->notify(new Telegram);
